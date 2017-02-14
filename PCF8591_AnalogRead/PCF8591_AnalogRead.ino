d3599272b586f50707ac30631d8a380cfd75fd8a
yevgeny hong <yev_science@naver.com>
2017-02-14 22:30:33 +0900

    Add files via upload

---
+++ PCF8591_AnalogRead/PCF8591_AnalogRead.ino
@@ -0,0 +1,21 @@
+#include <Wire.h>
+#include <MECHA_PCF8591.h>
+
+MECHA_PCF8591 pcf;
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.print(pcf.analogRead(0));
+  Serial.print(" ");
+  Serial.print(pcf.analogRead(1));
+  Serial.print(" ");
+  Serial.print(pcf.analogRead(2));
+  Serial.print(" ");
+  Serial.print(pcf.analogRead(3));
+  Serial.println();
+  delay(100);
+}
---
+++ PCF8591_AnalogWrite/PCF8591_AnalogWrite.ino
@@ -0,0 +1,21 @@
+#include <Wire.h>
+#include <MECHA_PCF8591.h>
+
+MECHA_PCF8591 pcf;
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i = 110; i < 150;i++){
+    pcf.analogWrite(i);
+    delay(10);
+  }
+  for(int i = 150; i > 110;i--){
+    pcf.analogWrite(i);
+    delay(10);
+  }  
+}
